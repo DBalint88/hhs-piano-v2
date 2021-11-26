2b7661ced72787f43e2ff1e13f879160d87363b8
DBalint88 <dbalint88@gmail.com>
2021-11-26 17:52:33 -0500

    random unsaved small changes

---
+++ hhs-piano-v2/layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div id="content-wrapper" class="bg-primary container">
+      <Header />
+      <Nav :currentPage="this.$route.path" />
+      <router-view />
+      <Footer />
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Header from "@/components/Header.vue";
+import Nav from "@/components/Nav.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  data() {
+    return {
+      currentPage: ''
+    }
+  },
+    components: {
+    Header,
+    Nav,
+    Footer
+  }
+}
+
+</script>
+
+<style lang="scss">
+@import './assets/css/base.normalize';
+@import './assets/css/settings.variables';
+@import './assets/css/settings.responsive';
+@import './assets/css/components.content';
+@import './assets/css/style.scss'
+
+
+</style>
